7de318bb4fa993328f71117a744ac302256a97a5
Skpow1234 <jfhvjfhv0015@gmail.com>
2026-03-31 22:16:53 -0500

    Add benchmark support for decoder in CMake configuration and implement a placeholder benchmark function in decoder_benchmark.cpp.

--- benchmarks/decoder_benchmark.cpp
+++ benchmarks/decoder_benchmark.cpp
@@ -1,4 +1,10 @@
-int main() {
-    return 0;
+#include <benchmark/benchmark.h>
+
+static void BM_decoder_placeholder(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(0);
+    }
 }
+
+BENCHMARK(BM_decoder_placeholder);
 
